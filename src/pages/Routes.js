78497dd1e9b8dae38e6a089b66523dd3df0ecc8c
HasnatGill <hasnatgill404@gmail.com>
2023-08-27 12:59:04 +0500

    add the Auth routing in Todo app

--- src/pages/Routes.js
+++ src/pages/Routes.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Frontend from './Frontend'
-
-
-
+import Auth from './Auth'
 
 export default function Index() {
     return (
         <>
                 <Routes>
                     <Route path='/*' element={<Frontend />} />
+                    <Route path='/auth/*' element={<Auth />} />
                 </Routes>
         </>
     )
